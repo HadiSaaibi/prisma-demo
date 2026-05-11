f9413dac453d9610745c88944c770853419c9ed0
HadiSaaibi <90275015+HadiSaaibi@users.noreply.github.com>
2026-05-11 15:37:45 +0300

    feat: add seed logic

--- seed.ts
+++ seed.ts
@@ -1,31 +1,50 @@
 import { faker } from "@faker-js/faker";
-
 import { prisma } from "./lib/prisma";
 
 async function main() {
+
   // Create a new author with a book
   const author = await prisma.author.create({
+    data: { name: faker.person.fullName(), email: faker.internet.email() },
+  });
+
+  const publisher = await prisma.publisher.create({
+    data: { name: faker.company.name() },
+  });
+
+  const genre = await prisma.genre.create({
+    data: { type: "Fiction" },
+  });
+
+  const user = await prisma.user.create({
+    data: { name: faker.person.fullName(), email: faker.internet.email() },
+  });
+
+  const book = await prisma.book.create({
     data: {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      books: {
-        create: {
-          title: faker.book.title(),
-        },
+      title: faker.book.title(),
+      authorId: author.id,
+      publisherId: publisher.id,
+      genres: {
+        create: { genreId: genre.id, isPrimary: true },
       },
-    },
-    include: {
-      books: true,
     },
   });
 
-  console.log("Created author:", author);
+  await prisma.UserReview.create({
+    data: {
+      rating: 5,
+      comment: faker.lorem.sentence(),
+      bookId: book.id,
+      userId: user.id,
+    },
+  });
+
+  console.log("done");
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
+  .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
     console.error(e);
     await prisma.$disconnect();
